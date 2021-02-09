7e72e6253661f5dc957b4d9abdf696f582ae57db
Alice Voytkevich <71379424+voytk@users.noreply.github.com>
2021-02-09 18:52:51 +0200

    v1

---
+++ 2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp32
+{
+    class Program
+    {
+        delegate int MyDelegat(int i, int n);
+
+        static void Main(string[] args)
+        {
+
+            MyDelegat a = (x, y) => x + y;
+            MyDelegat s = (x, y) => x - y;
+            MyDelegat m = (x, y) => x * y;
+            MyDelegat d = (x, y) => x / y;
+
+
+            int add = a(12, 23);
+
+            int sub = s(15, 10);
+
+            int mul = m(2, 3);
+
+            int del = d(4, 2);
+
+            Console.WriteLine("Додавання {0} Віднімання {1} Множення {2} Розподіл {3}", add, sub, mul, del);
+            Console.ReadKey();
+        }
+    }
+}
