186c8f5d08f73f6ebefde3909b670abc796e5857
Alice Voytkevich <71379424+voytk@users.noreply.github.com>
2021-02-09 18:47:51 +0200

    v1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        delegate int Anonim(int x, int y, int w);
+        static void Main(string[] args)
+        {
+            Anonim a = delegate (int x, int y, int w) { return (x + y + w) / 3; };
+            Console.WriteLine(a(2, 3, 4));
+            Console.ReadKey();
+        }
+    }
+}
